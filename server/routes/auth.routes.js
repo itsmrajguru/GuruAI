83413397e64bf7926476c762e4c463cf727411ac
Mangesh Rajguru <msrajguru7@gmail.com>
2026-03-21 21:46:13 +0530

    added signup Route

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const authRouter=express.Router()
+
+//importing controller
+const authController = require('../controllers/auth.controller');
+
+// User Authentication Routes
+authRouter.post('/signup/', authController.signup);
+module.exports={authRouter}
