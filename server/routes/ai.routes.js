41bba45d881d14589562a6a831e79633ade25b77
Mangesh Rajguru <msrajguru7@gmail.com>
2026-04-06 10:04:18 +0530

    added chatRoutes

---
+++ server/routes/ai.routes.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const aiRouter=express.Router()
+
+//importing middleware
+const {protect}=require('../middleware/auth.middleware')
+
+//importing controller 
+const {aiController}=require('../controllers/ai.controller')
+
+//routes for Gemini chat service integration
+
+//to post a message to gemini and get reply
+aiRouter.post('/chat',protect,aiController.chat)
+
+//to get all conversations to display in the sidebar
+aiRouter.get('/conversations',protect,aiController.getConversations)
+
+//to get a single conversation with full messages
+aiRouter.get('/conversation/:id',protect, aiController.getConversation)
+
+//to permentaly delete a conversation
+aiRouter.delete('/=conversation/:id',protect, aiController.deleteConversation)
+
+module.exports={aiRouter};
