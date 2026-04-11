51aec5cc4a5cda3288e566819022fddc4740907e
Mangesh Rajguru <msrajguru7@gmail.com>
2026-04-11 09:52:03 +0530

    fixed issues for deployment

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ require('dnscache')({
 // CORS configuration — strengthened for Production
 const allowedOrigins = [
     process.env.CLIENT_URL,
+    "https://guruaivercel.vercel.app",
     "https://guruaii.netlify.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173"
