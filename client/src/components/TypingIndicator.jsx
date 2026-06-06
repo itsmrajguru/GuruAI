a3234e519b2e1fe4a83db6d01fbd834ad8740c11
Mangesh Rajguru <msrajguru7@gmail.com>
2026-06-07 01:49:14 +0530

    fixed typinf indicator issue

--- client/src/components/TypingIndicator.jsx
+++ client/src/components/TypingIndicator.jsx
@@ -4,13 +4,13 @@
 export default function TypingIndicator() {
   return (
     <div style={{ display:'flex', gap:'16px', alignItems:'flex-start', padding:'18px 0' }}>
-      /* here we show the GuruAI logo right next to the typing dots */
+      {/* here we show the GuruAI logo right next to the typing dots */}
       <div style={{ width:'26px', height:'26px', borderRadius:'6px', background:'#fff', flexShrink:0, marginTop:'1px', overflow:'hidden', display:'flex', alignItems:'center', justifyContent:'center' }}>
         <img src="/logo.svg" alt="GuruAI" style={{ width:'100%', height:'100%', objectFit:'contain' }} />
       </div>
 
-      /* this container holds the three dots which will animate
-        one by one to create a nice bouncing effect */
+      {/* this container holds the three dots which will animate
+        one by one to create a nice bouncing effect */}
       <div style={{ flex:1, display:'flex', gap:'5px', alignItems:'center', height:'24px' }}>
         {[0, 200, 400].map(delay => (
           <span
