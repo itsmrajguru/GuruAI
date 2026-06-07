d030d7b0b52cfcc5ecf6316edc97c9bf1ccfee81
Mangesh Rajguru <msrajguru7@gmail.com>
2026-06-07 11:59:13 +0530

    fixed guruai urls

--- server/server.js
+++ server/server.js
@@ -17,9 +17,6 @@ require('dnscache')({
 // CORS configuration — strengthened for Production
 const allowedOrigins = [
     process.env.CLIENT_URL,
-    "https://guruaiplatform.vercel.app",
-    "https://guruaivercel.vercel.app",
-    "https://guruaii.netlify.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
